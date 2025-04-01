3aaf1aabab99d5db289a1334f07d07ee6d2ba9f8
Manisha Rana <manisharana02005@gmail.com>
2025-04-01 11:25:49 +0530

    created a todo object & inetrted it into db

---
+++ controllers/createTodo.js
@@ -0,0 +1,36 @@
+
+//creating a todo object & inetrting it into db
+
+const { response } = require("express");
+const Todo=require("../models/Todo");
+
+//define route handler
+exports.createTodo = async(req, res)=>{
+    try{
+        //extract title & description from request body
+        const {title, description}=req.body;
+
+        //create a new Todo obj and insert into DB
+        const res=await Todo.create({title, description});
+
+        //send a json response with a success flag
+        res.status(200).json(
+            {
+                success:true,
+                data:response,
+                message:'Entry created successfully'
+            }
+        )
+
+    }
+    catch(err){
+        console.error(err);
+        console.log(err);
+        res.status(500).json({
+            success:false,
+            data:"Internal server error",
+            message:err.message,
+        });
+    
+    }
+}
